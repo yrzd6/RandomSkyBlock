a4f1afc1f15faced6e58d71ee151ac68a63b0773
Youranzide <141908018+yrzd6@users.noreply.github.com>
2025-01-23 22:42:29 +0800

    新增 #timer，修改细节

--- script/main.js
+++ script/main.js
@@ -79,9 +79,11 @@ function Init() {
 function Start() {
 
     let Progress = parseInt((ChangeTimes - Timer + 1) / ChangeTimes * 12); // 计算进度
+    if(Progress < 0)
+        Progress = 0; // 防止溢出
+    
     let ProgressBar = "§a" + "█".repeat(Progress) + "§f" + "█".repeat(12 - Progress);
-
-    overworld.runCommand(`titleraw @a actionbar {"rawtext": [{"text":"距离方块变换还有 ${Timer} 秒。\n${ProgressBar}"}]}`);
+    overworld.runCommand(`titleraw @a actionbar {"rawtext": [{"text":"距离方块变换还有 ${Timer}/${ChangeTimes} 秒。\n${ProgressBar}"}]}`);
 
 
     Timer -= 1;
@@ -92,9 +94,8 @@ function Start() {
 };
 
 function CheckIsOp(player) { // 这个函数用于检测玩家是否是管理员，非管理员不能用 #c、#next 指令
-    return 1; 
+    return 1;
 }
-
 world.beforeEvents.chatSend.subscribe((msg) => {
     let IsSenderOp = CheckIsOp(msg.sender); // 检查是否是管理员
     if (msg.message == "#c") {
@@ -126,13 +127,29 @@ world.beforeEvents.chatSend.subscribe((msg) => {
     if (msg.message.startsWith(`#set `)) {
         msg.cancel = true;
         let NewTimes = parseFloat(msg.message.substring(5).trim()); // 获取新设置的时间
-        if (NewTimes < 1 || NewTimes > 32767) {
-            msg.sender.sendMessage(`参数值 ${NewTimes} 出现问题，请输入 1 至 32767 之间的整数（单位：秒）。`);
+        if (NewTimes < 1 || NewTimes > 65535) {
+            msg.sender.sendMessage(`参数值 ${NewTimes} 出现问题，请输入 1 至 65,535 之间的整数（单位：秒）。`);
             return;
         }
         ChangeTimes = NewTimes;
         Timer = ChangeTimes;
-        msg.sender.sendMessage(`成功设置变化时间为 ${ChangeTimes} 秒。`);
+        world.sendMessage(`成功设置变化时间为 ${ChangeTimes} 秒。`);
+    }
+    if (msg.message.startsWith(`#timer `)) {
+        msg.cancel = true;
+        if (IsSenderOp) {
+            let NewTimer = parseFloat(msg.message.substring(7).trim()); // 获取新设置的时间
+            if (NewTimer < 1 || NewTimer > 65535) {
+                msg.sender.sendMessage(`参数值 ${NewTimer} 出现问题，请输入 1 至 65,535 之间的整数（单位：秒）。`);
+                return;
+            }
+            Timer = NewTimer;
+            world.sendMessage(`成功设置计时器时间为 ${NewTimer} 秒。`);
+            world.sendMessage({ "translate": "item.boat.big_oak.name" });
+        }
+        else {
+            msg.sender.sendMessage("您不是管理员!");
+        }
     }
 });
 
